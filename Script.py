aba855c2956990183e36943a76dbb72975feeeba
TGDarkLord <113157573+TGDarkLord@users.noreply.github.com>
2022-10-17 04:49:02 +0530

    Add files via upload

---
+++ Script.py
@@ -0,0 +1,104 @@
+class script(object):
+    START_TXT = """<b><i>Hello 👋 {},
+
+I Am Not Only <a href="https://t.me/FilmZone_Robot">Film Zone Bot</a> To Assist You But Also Employed At <a href="https://t.me/+bTrA63X_d2I3MDVl">Film Zone</a> Group By <a href="https://t.me/hellodarklord">DarkLord</a> So You Can't Get My Service By Adding Me To Your Group So Don't Waste Your Time & Data 😉
+
+Better You Click Below & Join <a href="https://t.me/+bTrA63X_d2I3MDVl">Film Zone</a> & Feel The Experience Of Downloading Unlimited Movies/Series ✅
+
+For More Information Click ℹ️ Help</b></i>"""
+    
+    HELP_TXT = """<i><b>Hello 👋 {},</i></b>
+
+<i><b>I can Guide You Through All Of <a href="https://t.me/FilmZone_Robot">Film Zone Bot</a>'s Cool Features & How To Properly Use Them. Use The Buttons Below To Navigate Through All Of The Modules.</i></b>"""
+
+    ALIVE_TXT = """<b>Module :-</b> Alive
+
+- To Find Out If I'm Alive Or Not
+
+<b>📚 Available Command:</b>
+
+• /alive - Look
+
+<b>Maintained By ✔️ <a href="https://t.me/hellodarklord">DarkLord</a></b>"""
+
+    LINK_TXT = """<b>Module :-</b> Invite Link
+
+- You Can Easily Find Group Invitation Link
+
+<b>📚 Available Command:</b>
+
+• /link - To Get Movie Club Group Link
+
+<b>Maintained By ✔️ <a href="https://t.me/hellodarklord">DarkLord</a></b>"""
+   
+    INFO_TXT = """<b>Module :-</b> IDs
+
+- A Module To Fetch Telegram User, Group, Channel Info
+
+<b>📚 Available Commands:</b>
+• /id - get id of a specified user.
+• /info  - get information about a user.
+
+<b>Maintained By ✔️ <a href="https://t.me/hellodarklord">DarkLord</a></b>""" 
+
+    SEARCH_TXT = """<b>Module :-</b> Imdb Info
+
+- A Module To Get The Movie Informations. Use This Module To Get Movie Informations
+
+<b>📚 Available Commands:</b>
+• /imdb  - get the film information from IMDb source.
+• /search  - get the film information from various sources.
+
+<b>Maintained By ✔️ <a href="https://t.me/hellodarklord">DarkLord</a></b>""" 
+
+    MUSIC_TXT = """<b>Module :-</b> Song
+
+- Music download modules, for those who love music.
+
+<b>📚 Available Commands:</b>
+• /song (songname) - Download Song From Film Zone Server.
+• /video (songname) - Download Video From Film Zone Server.
+
+<b>Maintained By ✔️ <a href="https://t.me/hellodarklord">DarkLord</a></b>""" 
+    
+    BIO_TXT = """<i><b>Hello 👋 {},
+I Can't Write Everything In My Bio So I Created This...</i>
+
+╭──[─═ ᴘᴇʀꜱᴏɴᴀʟ ɪɴꜰᴏ ═─]
+├• Name⇝ Bikash Maity
+├• Gender⇝ Male
+├• TG Name⇝ <a href="https://t.me/hellodarklord">DarkLord</a>
+├• Username⇝ @hellodarklord
+├• Birthday⇝ 10th January 2006
+├• Age⇝ 16
+├• Relationship Status⇝ Single
+├• Nationality⇝ Indian
+├• Location⇝ West Bengal, Manikpur
+├• Contact Bot⇝ Soon...🤖
+╰──────⍟───────╯
+
+╭──[─═ ᴏᴡɴᴇʀ ᴀᴛ ═─]
+├• <a href="https://t.me/+bTrA63X_d2I3MDVl">FILM ZONE</a>
+├• <a href="https://t.me/+dB60Z_TJ2JswZGY9">FILM ZONE UPDATES</a>
+├• <a href="https://t.me/FilmZone_Robot">FILM ZONE BOT</a>
+╰──────⍟───────╯
+
+<i>Thank You For Reading Patiently 🙏</i></b>"""
+
+    DONATE_TXT = """<i><b>Hello 👋 {},
+
+Thank You For Showing Interest In My Works 🙏
+
+To Support My Works, Please Feel Free To Donate Any Amount You Like 💸
+
+There Are Multiple Ways To Donate By Clicking On The Buttons Below</i></b>"""
+
+    LOG_TEXT_G = """#NewGroup
+Group = {}(<code>{}</code>)
+Total Members = <code>{}</code>
+Added By - {}
+"""
+    LOG_TEXT_P = """#NewUser
+ID - <code>{}</code>
+Name - {}
+"""
