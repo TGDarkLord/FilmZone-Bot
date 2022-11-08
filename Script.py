76bbc4e1249d3b77bf4bcba9d45407a076209bb9
TGDarkLord <113157573+TGDarkLord@users.noreply.github.com>
2022-11-08 17:10:39 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,7 +1,7 @@
 class script(object):
     START_TXT = """<b><i>Hello 👋 {},
 
-I Am Not Only <a href="t.me/MovieTimeMovie_Bot">Movie Time Bot</a> To Assist You But Also Employed At <a href="https://t.me/+_eNFsILCRAA0Njll">Movie Time</a> Group By <a href="https://t.me/hellobrokenheart">BrokenHeart</a> So You Can't Get My Service By Adding Me To Your Group So Don't Waste Your Time & Data 😉
+I Am Not Only <a href="t.me/MovieTimeMovie_Bot">Movie Time Bot</a> To Assist You But Also Employed At <a href="https://t.me/+_eNFsILCRAA0Njll">Movie Time</a> Group By <a href="https://t.me/hellobrokenheart">Heart</a> So You Can't Get My Service By Adding Me To Your Group So Don't Waste Your Time & Data 😉
 
 Better You Click Below & Join <a href="https://t.me/+_eNFsILCRAA0Njll">Movie Time</a> & Feel The Experience Of Downloading Unlimited Movies/Series ✅
 
@@ -19,7 +19,7 @@ For More Information Click ℹ️ Help</b></i>"""
 
 • /alive - Look
 
-<b>Maintained By ✔️ <a href="t.me/hellobrokenheart">BrokenHeart</a></b>"""
+<b>Maintained By ✔️ <a href="t.me/hellobrokenheart">Heart</a></b>"""
 
     LINK_TXT = """<b>Module :-</b> Invite Link
 
@@ -29,7 +29,7 @@ For More Information Click ℹ️ Help</b></i>"""
 
 • /link - To Get Movie Club Group Link
 
-<b>Maintained By ✔️ <a href="https://t.me/hellobrokenheart">BrokenHeart</a></b>"""
+<b>Maintained By ✔️ <a href="https://t.me/hellobrokenheart">Heart</a></b>"""
 
     INFO_TXT = """<b>Module :-</b> IDs
 
@@ -39,7 +39,7 @@ For More Information Click ℹ️ Help</b></i>"""
 • /id - get id of a specified user.
 • /info  - get information about a user.
 
-<b>Maintained By ✔️ <a href="https://t.me/hellobrokenheart">BrokenHeart</a></b>"""
+<b>Maintained By ✔️ <a href="https://t.me/hellobrokenheart">Heart</a></b>"""
 
     SEARCH_TXT = """<b>Module :-</b> Imdb Info
 
@@ -49,7 +49,7 @@ For More Information Click ℹ️ Help</b></i>"""
 • /imdb  - get the film information from IMDb source.
 • /search  - get the film information from various sources.
 
-<b>Maintained By ✔️ <a href="https://t.me/hellobrokenheart">BrokenHeart</a></b>"""
+<b>Maintained By ✔️ <a href="https://t.me/hellobrokenheart">Heart</a></b>"""
 
     MUSIC_TXT = """<b>Module :-</b> Song
 
@@ -59,7 +59,7 @@ For More Information Click ℹ️ Help</b></i>"""
 • /song (songname) - Download Song From Film Zone Server.
 • /video (songname) - Download Video From Film Zone Server.
 
-<b>Maintained By ✔️ <a href="https://t.me/hellobrokenheart">BrokenHeart</a></b>"""
+<b>Maintained By ✔️ <a href="https://t.me/hellobrokenheart">Heart</a></b>"""
     
     BIO_TXT = """<i><b>Hello 👋 {},
 I Can't Write Everything In My Bio So I Created This...</i>
@@ -67,7 +67,7 @@ I Can't Write Everything In My Bio So I Created This...</i>
 ╭──[─═ ᴘᴇʀꜱᴏɴᴀʟ ɪɴꜰᴏ ═─]
 ├• Name⇝ Bikash Maity
 ├• Gender⇝ Male
-├• TG Name⇝ <a href="t.me/hellobrokenheart">BrokenHeart</a>
+├• TG Name⇝ <a href="t.me/hellobrokenheart">Heart</a>
 ├• Username⇝ @hellobrokenheart
 ├• Birthday⇝ 10th January 2006
 ├• Age⇝ 16
